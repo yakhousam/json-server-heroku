dcf07222daa6da8b01fbaf75e8b3da031d680078
yakhousam <42283612+yakhousam@users.noreply.github.com>
2020-08-25 13:05:09 +0100

    use custom routes

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+const rewriteRoutes = require("./routes.json")
 
+server.use(jsonServer.rewriter(rewriteRoutes))
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, ()=>console.log("sever is runing on port:", port));
